cb74afa2a14d506e438267273c9cfda2510f3fb5
sexusabortus123 <dotafor225@gmail.com>
2025-09-03 23:55:30 +0900

    server.js

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const WebSocket = require('ws');
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, 'index.html');
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Ошибка сервера');
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  });
-});
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
 
-const wss = new WebSocket.Server({ server });
-
-let clients = new Set();
+const clients = new Set();
 
 wss.on('connection', (ws) => {
   clients.add(ws);
+  console.log('New client connected');
 
   ws.on('message', (message) => {
+    console.log('Received:', message);
     // Рассылаем сообщение всем подключенным клиентам
-    for (let client of clients) {
-      if (client.readyState === WebSocket.OPEN) {
+    for (const client of clients) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     }
@@ -33,10 +21,8 @@ wss.on('connection', (ws) => {
 
   ws.on('close', () => {
     clients.delete(ws);
+    console.log('Client disconnected');
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-});
+console.log(`WebSocket server started on port ${PORT}`);
